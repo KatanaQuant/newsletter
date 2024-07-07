077d84a19386ccf7c4b5d85c94f02694d369f2b2
Horoshi <horoshi@katanaquant.com>
2024-07-07 10:51:55 +0200

    adds branding colors

--- Issue 2 - Sharpe Ratio/BTC_stdDev_calc.py
+++ Issue 2 - Sharpe Ratio/BTC_stdDev_calc.py
@@ -166,48 +166,52 @@ print(f'Within 2 Standard Deviations (95% probability):')
 print(f'Lower Bound: {lower_bound_95*100: .2f}%')
 print(f'Upper Bound: {upper_bound_95*100: .2f}%')
 
+plt.figure(facecolor='#f7e9e1')
 
 # Plotting the histogram
 plt.hist(daily_returns*100, bins=50, alpha=0.7, density=True,
-         label='Daily Returns', histtype='bar', rwidth=0.8, color='skyblue')
+         label='Daily Returns', histtype='bar', rwidth=0.8, color='#413b3c')
 
 # Calculating x and y values for the normal distribution curve
 x = np.linspace(min(daily_returns*100), max(daily_returns*100), 100)
 y = norm.pdf(x, average_daily_return*100, std_dev*100)
 
 # Plotting the normal distribution curve
-plt.plot(x, y, 'r-', label='Normal Distribution', linewidth=2)
+plt.plot(x, y, color='#de4d39', label='Normal Distribution', linewidth=2)
 
 # Adding vertical lines for the average and bounds
-plt.axvline(average_daily_return*100, color='green',
+plt.axvline(average_daily_return*100, color='#100d16',
             linestyle='--', linewidth=2, label='Average Daily Return')
-plt.axvline(lower_bound_68*100, color='orange', linestyle='--',
+plt.axvline(lower_bound_68*100, color='#de4d39', linestyle='--',
             linewidth=2, label='Lower Bound 68%')
-plt.axvline(upper_bound_68*100, color='orange', linestyle='--',
+plt.axvline(upper_bound_68*100, color='#de4d39', linestyle='--',
             linewidth=2, label='Upper Bound 68%')
-plt.axvline(lower_bound_95*100, color='red', linestyle='--',
+plt.axvline(lower_bound_95*100, color='#de4d39', linestyle='--',
             linewidth=2, label='Lower Bound 95%')
-plt.axvline(upper_bound_95*100, color='red', linestyle='--',
+plt.axvline(upper_bound_95*100, color='#de4d39', linestyle='--',
             linewidth=2, label='Upper Bound 95%')
 
 # Annotating the vertical lines with their literal values, adjusted to plot lower on the y-axis
 plt.text(average_daily_return*100, plt.ylim()
-         [1]*0.45, f'{average_daily_return*100:.2f}%', ha='right')
+         [1]*0.45, f'{average_daily_return*100:.2f}%', ha='right', color='#100d16')
 plt.text(lower_bound_68*100, plt.ylim()
-         [1]*0.40, f'{lower_bound_68*100:.2f}%', ha='right')
+         [1]*0.40, f'{lower_bound_68*100:.2f}%', ha='right', color='#100d16')
 plt.text(upper_bound_68*100, plt.ylim()
-         [1]*0.40, f'{upper_bound_68*100:.2f}%', ha='left')
+         [1]*0.40, f'{upper_bound_68*100:.2f}%', ha='left', color='#100d16')
 plt.text(lower_bound_95*100, plt.ylim()
-         [1]*0.45, f'{lower_bound_95*100:.2f}%', ha='right')
+         [1]*0.45, f'{lower_bound_95*100:.2f}%', ha='right', color='#100d16')
 plt.text(upper_bound_95*100, plt.ylim()
-         [1]*0.45, f'{upper_bound_95*100:.2f}%', ha='left')
+         [1]*0.45, f'{upper_bound_95*100:.2f}%', ha='left', color='#100d16')
 
 # Customizing the plot with enhanced labels, title, and grid
-plt.xlabel('Daily Returns (%)', fontsize=12)
-plt.ylabel('Probability Density', fontsize=12)
-plt.title('Daily BTC/USDT Returns stdDev +1, +2', fontsize=14)
+plt.xlabel('Daily Returns (%)', fontsize=12, color='#100d16')
+plt.ylabel('Probability Density', fontsize=12, color='#100d16')
+plt.title('Daily BTC/USDT Returns stdDev +1, +2', fontsize=14, color='#100d16')
 plt.legend()
-plt.grid(True, which='both', linestyle='--', linewidth=0.5)
+
+plt.grid(True, which='both', linestyle='--', linewidth=0.5, color='#100d16')
+
+plt.tick_params(colors='#100d16')
 
 # Saving the figure with improved styling
 plt.savefig('daily_btc_stdDev.png')
